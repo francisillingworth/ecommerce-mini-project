a63359691b9f82a14a4faa5a96842d7cd255bd3f
Ubuntu <ubuntu@ip-172-31-83-11.ec2.internal>
2019-07-23 15:17:14 +0000

    commit for help from tutor support

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
-from django.db.models import Q
 
 
-class CaseInsensitiveAuth:
+
+class EmailAuth:
     """Authenticate a user by an exact match on the email and password"""
 
     def authenticate(self, username=None, password=None):
